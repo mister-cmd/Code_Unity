38335e7195a83f37be8e9c8fa6ceae831e35f50e
mister-cmd <smyrinvladislav52@gmail.com>
2024-11-17 15:14:55 +0500

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //Здоровье NPC
+    int health = 5;
+    //Уровень NPC
+    int level = 1;
+    //Скорость NPC
+    float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = health + level;
+        print("Жизней: " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = health + level;
+        print("Жизней: " + health);
+    }
+}
