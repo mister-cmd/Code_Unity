e50a5ee826365a7fb095f0cce01b16a41dd143c5
mister-cmd <smyrinvladislav52@gmail.com>
2024-11-17 15:37:50 +0500

    Update NPC.cs

--- NPC.cs
+++ NPC.cs
@@ -21,6 +21,11 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Создаём переменную вида Vector3 и сохраняем в неё позицию NPC
+        Vector3 newPosition = transform.position;
+         //Меняем позицию NPC по оси z согласно скорости NPC и времени между кадрами
+        newPosition.z += speed * Time.deltaTime;
+         //Меняем позицию NPC на новое значение, рассчитанное выше
+        transform.position = newPosition;
     }
 }
