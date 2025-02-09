4b9d2b58826a085db864facafd9bd7ac0fc82076
mister-cmd <smyrinvladislav52@gmail.com>
2025-02-09 15:39:14 +0500

    Update Projectile.cs

--- Projectile.cs
+++ Projectile.cs
@@ -27,27 +27,3 @@ public class Projectile : MonoBehaviour
         }
     }
 }
-GameController.cs
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
- 
-public class GameController : MonoBehaviour
-{
-    public static float gameSpeed;
- 
-    [Range(0, 5)]
-    public float gameSpeedRegulator;
-    public float speedRate = 0.5f;
-    public float gameSpeedMax = 5;
- 
-    void Update()
-    {
-        if (gameSpeedRegulator <= gameSpeedMax)
-        {
-            gameSpeedRegulator += speedRate * Time.deltaTime;
-        }
-        gameSpeed = gameSpeedRegulator;
-    }
-}
